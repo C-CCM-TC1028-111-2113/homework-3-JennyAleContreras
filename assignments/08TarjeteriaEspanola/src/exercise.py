56d1459e9e6793359eb0937f31756d389a86a97f
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-28 20:51:00 -0500

    Update exercise.py

--- assignments/08TarjeteriaEspanola/src/exercise.py
+++ assignments/08TarjeteriaEspanola/src/exercise.py
@@ -1,16 +1,16 @@
+def calculate_tarjetas(pliegos, plumones):
+    global plieg, plum
+    plieg=(pliegos*12)
+    plum=(plumones*35)
+    return min(plieg, plum)
+    
 def main():
     #escribe tu código abajo de esta línea
 
-    def calculate_tarjetas(pliegos, plumones):
-        global plieg, plum
-        plieg=(pliegos*12)
-        plum=(plumones*35)
-        return min(plieg, plum)
-
     pliegos = int(input('Dame la cantidad de pliegos de papel albanene: '))
     plumones = int(input('Dame la cantidad de plumones: '))
-
     print('El número máximo de tarjetas que se pueden hacer es:',calculate_tarjetas(pliegos, plumones))
+    
     pass
 
 if __name__=='__main__':
