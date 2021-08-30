2b03e5f0edcf444b4478c54708e26da165b5f0c3
JennyAleContreras <88851067+JennyAleContreras@users.noreply.github.com>
2021-08-29 21:08:14 -0500

    Update exercise.py

--- assignments/07VolumenPrismaRectangular/src/exercise.py
+++ assignments/07VolumenPrismaRectangular/src/exercise.py
@@ -2,6 +2,7 @@ def calculate_area(base, profundidad):
     return (base*profundidad)
 
 def calculate_volume(altura):
+    calculate_area(base, profundidad)
     return (calculate_area(base, profundidad)*altura)
 
 def main():
